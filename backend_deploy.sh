8ecf660a21d89a89703c48a766e6e0e24ea49388
“wheatandcat” <wheatandcat@gmail.com>
2017-12-12 02:58:36 +0900

    fix

--- backend_deploy.sh
+++ backend_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /app/dotstamp_graphql
+cd /app/dotstamp_graphql_deploy/dotstamp_graphql
 git pull origin master
 docker build --tag="dotstamp_graphql:latest" .
 
-cd /app/
+cd /app/dotstamp_graphql_deploy/
 
 docker-compose up -d app
